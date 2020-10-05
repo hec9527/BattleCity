39d29cdd8466ad19e80751fa904f230df39d6f42
hec <hec9527@foxmail.com>
2020-10-05 11:27:35 +0800

    test: add map test

--- src/map/index.ts
+++ src/map/index.ts
@@ -3,13 +3,38 @@
  * @author  hec9527
  */
 
+import Print from '@src/util/print';
 import maps from './mapDatas';
 
 type mapData = Array<Array<number>>;
 
-class Maps {
-  private readonly maps: mapData[] = [[], [...maps]];
+export class Maps {
+  private readonly maps: mapData[] = [[], ...maps];
   private isCustomRound = false;
+
+  /**
+   * 获取真实的回合数
+   * - 回合数为0  --> 自定义地图
+   * - 回合数<35  --> 对应关卡
+   * - 回合数>35  --> 取模
+   *   -  35 --> 35
+   *   -  36 --> 1
+   *   -  70 --> 35
+   * @param fakeRound
+   */
+  private getRealRound(fakeRound: number): number | never {
+    let mod = fakeRound % 35;
+    if (fakeRound < 0) {
+      throw new Error('game round should greater or equal 0');
+    }
+    if (fakeRound === 0 && !this.isCustomRound) {
+      throw new Error('no custom map found, edit it first!');
+    }
+    if (fakeRound <= 35) {
+      return fakeRound;
+    }
+    return mod === 0 ? 35 : mod;
+  }
 
   /**
    * 获取关卡数据
@@ -17,28 +42,31 @@ class Maps {
    * 如果给出的 round 大于总关卡数，则取余数
    * @param round 关卡数
    */
-  getMapData(round: number): mapData {
-    return this.fixMapData(this.maps[round]);
+  public getMapData(round: number): mapData | false {
+    try {
+      const realRound = this.getRealRound(round);
+      return this.fixMapData(this.maps[realRound]);
+    } catch (error) {
+      Print.error(error);
+    }
+    return false;
   }
 
   /**
    * 设置自定义地图
    * @param mapData mapData
    */
-  setCustomRound(mapData: mapData): void {
+  public setCustomRound(mapData: mapData): void | never {
+    if (mapData.length !== 13) {
+      throw new Error('each custome map row should have length 13');
+    }
+    for (let row = 0; row < 13; row++) {
+      if (mapData[row].length !== 13) {
+        throw new Error('each custome map col should have length 13');
+      }
+    }
     this.isCustomRound = true;
     this.maps[0] = mapData;
-  }
-
-  /**
-   * 获取自定义地图数据
-   */
-  getCustomMapData(): mapData | false {
-    if (!this.isCustomRound) {
-      return false;
-    } else {
-      return this.fixMapData(this.maps[0]);
-    }
   }
 
   /**
@@ -57,4 +85,4 @@ class Maps {
   }
 }
 
-export default Maps;
+export default new Maps();
---
+++ test/map/map.test.ts
@@ -0,0 +1,97 @@
+import map, { Maps } from '@src/map/index';
+import mapData from '@src/map/mapDatas';
+import { getType } from '@src/util';
+
+const mockMap = new Array(13);
+
+for (let i = 0; i < 13; i++) {
+  mockMap[i] = new Array(13).fill(0);
+}
+
+describe('Maps class test', () => {
+  test('map should be instanceof Maps', () => {
+    expect(map).toBeInstanceOf(Maps);
+  });
+
+  test('map.getMapData should return false', () => {
+    expect(map.getMapData(0)).toBeFalsy();
+    expect(map.getMapData(-1)).toBeFalsy();
+  });
+
+  test('map.getMapData should return mapData', () => {
+    const _map = map.getMapData(1);
+    expect(_map).toHaveLength(13);
+    for (let i = 0; i < 13; i++) {
+      expect(_map && _map[i]).toHaveLength(13);
+    }
+  });
+
+  // 测试异常抛出  触发异常的函数需要用函数包起来
+  test('map.setCustomRound should throw error', () => {
+    expect(() => map.setCustomRound([])).toThrowError();
+    expect(() => map.setCustomRound(new Array(13).fill([]))).toThrowError();
+  });
+
+  test('map.getMapData[0] should return mapData after map.setCustomRound', () => {
+    map.setCustomRound(mockMap);
+    const _map = map.getMapData(0);
+    expect(_map).toBeTruthy();
+    for (let i = 0; i > 13; i++) {
+      expect(_map && _map[i]).toHaveLength(13);
+    }
+  });
+
+  test('map.getMapData(36) should equal map.getMapdata(1)', () => {
+    expect(map.getMapData(36)).toStrictEqual(map.getMapData(1));
+  });
+
+  test('map.getMapData(70) should equal map.getMapdata(35)', () => {
+    expect(map.getMapData(70)).toStrictEqual(map.getMapData(35));
+  });
+
+  test('map should fix mapData', () => {
+    const _map = map.getMapData(0);
+    expect(_map).toBeDefined();
+    expect(_map).toBeTruthy();
+    // console.log('%c地图数据', 'color:#abf', _map);
+
+    if (_map) {
+      expect(_map[11][5]).toBe(18);
+      expect(_map[11][6]).toBe(4);
+      expect(_map[11][7]).toBe(17);
+      expect(_map[12][5]).toBe(3);
+      expect(_map[12][6]).toBe(15);
+      expect(_map[12][7]).toBe(5);
+    }
+  });
+});
+
+describe('mapData should be defined correctly', () => {
+  test('mapData should be defined', () => {
+    expect(mapData).toBeDefined();
+  });
+
+  test('mapData should instanceof Array', () => {
+    expect(getType(mapData)).toBe('Array');
+  });
+
+  describe("mapData's element should be defined correctly", () => {
+    for (let i = 0, len = mapData.length; i < len; i++) {
+      const _map = mapData[i];
+
+      test('_map should be defined', () => {
+        expect(_map).toBeDefined();
+      });
+
+      test('_map should have length 13', () => {
+        expect(_map).toHaveLength(13);
+      });
+
+      test("_map's row should have length 13", () => {
+        for (let j = 0; j < 13; j++) {
+          expect(_map[j]).toHaveLength(13);
+        }
+      });
+    }
+  });
+});
