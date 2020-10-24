658c8b00ddbab4e6cbd10baf5e4f1bd82a116b72
hec <hec9527@foxmail.com>
2020-10-24 14:28:30 +0800

    update: 添加常量,修改entity类

---
+++ src/config/const.ts
@@ -0,0 +1,21 @@
+/**
+ * 相关常量
+ */
+
+/** canvas画布的 宽度 */
+export const GAME_CANVAS_WIDTH = 516;
+
+/** canvas 画布的 高度 */
+export const GAME_CANVAS_HEIGHT = 456;
+
+/** 游戏坦克活动区域的 宽度 */
+export const GAME_BATTLEFIELD_WIDTH = 416;
+
+/** 游戏坦克活动区域的 高度 */
+export const GAME_BATTLEFIELD_HEIGHT = 416;
+
+/** 游戏坦克活动区域的padding TOP */
+export const GAME_BATTLEFIELD_PADDING_TOP = 20;
+
+/** 游戏坦克活动区域的padding LEFT */
+export const GAME_BATTLEFIELD_PADDING_LEFT = 35;
--- src/object/entity.ts
+++ src/object/entity.ts
@@ -4,13 +4,22 @@
  */
 
 import { getCanvas } from '../util/index';
+import {
+  GAME_BATTLEFIELD_PADDING_LEFT as LEFT,
+  GAME_BATTLEFIELD_PADDING_TOP as TOP,
+} from '../config/const';
 
 abstract class Entity {
   protected canvas: HTMLCanvasElement;
   protected ctx: CanvasRenderingContext2D;
   public isCollision: boolean = true;
 
-  constructor(protected world: GameWorld, public rect: EntityRect, protected img: CanvasImageSource, public camp: Camp = 'neutral') {
+  constructor(
+    protected world: GameWorld,
+    public rect: EntityRect,
+    protected img: CanvasImageSource,
+    public camp: Camp = 'neutral'
+  ) {
     const { canvas, ctx } = getCanvas(516, 456, 'canvas');
     this.canvas = canvas;
     this.ctx = ctx;
@@ -26,7 +35,7 @@ abstract class Entity {
   }
 
   protected draw() {
-    this.ctx.drawImage(this.img, this.rect[0] + 35, this.rect[1] + 20);
+    this.ctx.drawImage(this.img, this.rect[0] + LEFT, this.rect[1] + TOP);
   }
 }
 
