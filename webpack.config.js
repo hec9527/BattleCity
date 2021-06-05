8752e002a70e639e8624160c6197c533aaac8741
hec9527 <hec9527@foxmail.com>
2021-06-05 09:21:39 +0800

    fix: 修复在chrome浏览器中, GPU memory占用过高, 导致页面异常卡顿的bug
    bug commit: 4268f621c26e02564ddb63f8752d274fcd6d0ca8

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,7 @@ const config = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+        // type: 'asset/resource',
       },
       {
         test: /\.(png|jpg|mp3|ttf)$/,
