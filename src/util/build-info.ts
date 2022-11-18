3ea6741edcfab2edcd7b6fa95a645eef8b5be351
hec9527 <hec9527@foxmail.com>
2022-11-18 12:13:15 +0800

    fix: 打包后环境变量报错的问题

--- src/util/build-info.ts
+++ src/util/build-info.ts
@@ -1,6 +1,14 @@
 export default undefined;
-
+const time = document.querySelector('meta[name="__BUILD_TIME"]') as HTMLMetaElement | null;
+const version = document.querySelector('meta[name="__BUILD_VERSION"]') as HTMLMetaElement | null;
 const copyright = (document.querySelector('#copy-right') as HTMLDivElement) || null;
+
+if (time) {
+  time.content = import.meta.env.BUILD_TIME || window.BUILD_TIME || '';
+}
+if (version) {
+  version.content = import.meta.env.BUILD_VERSION || window.BUILD_VERSION || '';
+}
 
 if (copyright) {
   copyright.title = import.meta.env.BUILD_VERSION || window.BUILD_VERSION || '';
